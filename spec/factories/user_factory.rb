979fdf322617ade89295971083e5d35f0217acc7
masayoshi-toku <39050027+masayoshi-toku@users.noreply.github.com>
2018-06-08 15:31:48 +0900

    Add factory girl and refactor test codes

---
+++ spec/factories/user_factory.rb
@@ -0,0 +1,6 @@
+FactoryGirl.define do
+  factory :user do
+    name "example"
+    email "example@mwed.co.jp"
+  end
+end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,22 +2,30 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   describe 'バリデーション' do
-    let(:user) { User.new }
+    context '正しい値の時' do
+      let(:user) { build(:user) }
+      it { expect(user).to be_valid }
+    end
 
     context '名前が空の時' do
-      it '作成できない' do
-        user.email = 'hoge@example.com'
-        user.name = ''
-        expect(user.save).to be false
-      end
+      let(:empty_name_user) { build(:user, name: '') }
+      it { expect(empty_name_user).to be_invalid }
     end
 
     context 'メールアドレスが空の時' do
-      it '作成できない' do
-        user.email = ''
-        user.name = 'hoge'
-        expect(user.save).to be false
-      end
+      let(:empty_email_user) { build(:user, email: '') }
+      it { expect(empty_email_user).to be_invalid }
+    end
+
+    context '不正なドメインの時' do
+      let(:invalid_email_user) { build(:user, email: 'example@gmail.com') }
+      it { expect(invalid_email_user).to be_invalid }
+    end
+
+    context '重複したアカウントがある時' do
+      let(:user) { create(:user) }
+      let(:dup_user) { user.dup }
+      it { expect(dup_user).to be_invalid }
     end
   end
 end
