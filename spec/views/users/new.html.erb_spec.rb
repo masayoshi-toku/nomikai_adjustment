48a99f3ab40f05de595effc3b3fe4e9a0488be10
masayoshi-toku <39050027+masayoshi-toku@users.noreply.github.com>
2018-06-08 15:33:43 +0900

    Specify the link to check for circle ci

--- spec/views/users/new.html.erb_spec.rb
+++ spec/views/users/new.html.erb_spec.rb
@@ -3,6 +3,6 @@ require 'rails_helper'
 RSpec.describe "users/new", type: :view do
   it "renders new" do
     render
-    expect(rendered).to have_link(href: '/auth/google_oauth2')
+    expect(rendered).to have_link('Googleアカウントでサインアップ', href: '/auth/google_oauth2')
   end
 end
