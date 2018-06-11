ac73828ddfbad41f46edb026fad46326643dfc75
masayoshi-toku <39050027+masayoshi-toku@users.noreply.github.com>
2018-06-11 11:37:16 +0900

    Use better name and email for sample variable

--- spec/views/users/edit.html.erb_spec.rb
+++ spec/views/users/edit.html.erb_spec.rb
@@ -3,8 +3,8 @@ require 'rails_helper'
 RSpec.describe "users/edit", type: :view do
   before(:each) do
     @user = assign(:user, User.create!(
-      :name => "MyString",
-      :email => "sth@mwed.co.jp"
+      :name => "Mr.example",
+      :email => "example@mwed.co.jp"
     ))
   end
 
