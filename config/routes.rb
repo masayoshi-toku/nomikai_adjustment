d0d7d3d575420d2c3beb56e48e145dc38e683b1d
masayoshi-toku <39050027+masayoshi-toku@users.noreply.github.com>
2018-06-11 15:25:23 +0900

    Create session model and controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :users
-  get '/auth/:provider/callback', to: 'users#create'
   get "/auth/google_oauth2", as: "google_auth"
+  get '/auth/:provider/callback', to: 'users#create'
+  get '/login', to: 'sessions#new'
 end
