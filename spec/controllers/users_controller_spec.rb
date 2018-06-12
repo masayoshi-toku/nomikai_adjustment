9ac909aafd956a910538e294279ddcc9e6bed764
masayoshi-toku <39050027+masayoshi-toku@users.noreply.github.com>
2018-06-12 15:28:48 +0900

    Modify spec files

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,63 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe UsersController, type: :controller do
-  include GoogleOmniauthHelper
-
   describe "GET #new" do
     before { get :new }
     it { expect(response).to be_success }
-  end
-
-  describe "POST #create" do
-    context "正しいパラメーターの場合" do
-      before do
-        valid_attributes = { name: 'Mr.example', email: 'example@mwed.co.jp', domain: 'mwed.co.jp' }
-        request.env['omniauth.auth'] = google_mock(valid_attributes)
-      end
-
-      it "ユーザー作成に成功する" do
-        expect { post :create }.to change(User, :count).by(1)
-      end
-
-      it "ユーザーの詳細ページへリダイレクトされる" do
-        post :create
-        created_user = User.find_by(email: 'example@mwed.co.jp')
-        expect(response).to redirect_to created_user
-      end
-    end
-
-    context "不正なパラメーターの場合" do
-      subject { response }
-
-      context "名前が空である" do
-        before do
-          invalid_attributes = { name: '', email: 'example@mwed.co.jp', domain: 'mwed.co.jp' }
-          request.env['omniauth.auth'] = google_mock(invalid_attributes)
-          post :create
-        end
-
-        it { is_expected.to redirect_to new_user_url }
-      end
-
-      context "メールアドレスが空である" do
-        before do
-          invalid_attributes = { name: 'Mr.example', email: '', domain: 'mwed.co.jp' }
-          request.env['omniauth.auth'] = google_mock(invalid_attributes)
-          post :create
-        end
-
-        it { is_expected.to redirect_to new_user_url }
-      end
-
-      context "ドメインが不正である" do
-        before do
-          invalid_attributes = { name: 'Mr.example', email: 'example@gmail.com', domain: 'gmail.com' }
-          request.env['omniauth.auth'] = google_mock(invalid_attributes)
-          post :create
-        end
-
-        it { is_expected.to redirect_to new_user_url }
-      end
-    end
   end
 end
--- spec/views/users/new.html.erb_spec.rb
+++ spec/views/users/new.html.erb_spec.rb
@@ -1,8 +1,4 @@
 require 'rails_helper'
 
 RSpec.describe "users/new", type: :view do
-  it "renders new" do
-    render
-    expect(rendered).to have_link('Googleアカウントでサインアップ', href: google_auth_path)
-  end
 end
