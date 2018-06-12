e6f34921067660b276272c44df4e7adbcb8a5053
masayoshi-toku <39050027+masayoshi-toku@users.noreply.github.com>
2018-06-12 15:29:09 +0900

    Add find_or_new_from method

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,12 @@ class User < ApplicationRecord
   validates :name, presence: true
   VALID_DOMAIN_REGEX = /@mwed.co.jp\z/
   validates :email, presence: true, uniqueness: true, format: { with: VALID_DOMAIN_REGEX }
+
+  def self.find_or_new_from(user_params)
+    user = User.find_by(email: user_params[:email])
+    unless user
+      user = User.new(user_params)
+    end
+    return user
+  end
 end
