9b01c64a1a5e6c84ea34459decf01cf993fc4672
masayoshi-toku <39050027+masayoshi-toku@users.noreply.github.com>
2018-06-13 19:21:27 +0900

    Add relationship between user and event

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,4 @@
 class Event < ApplicationRecord
   validates :title, presence: true, length: { maximum: 100 }, uniqueness: { scope: [:user_id, :title]  }
+  belongs_to :user
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,5 @@ class User < ApplicationRecord
   validates :name, presence: true
   VALID_DOMAIN_REGEX = /@mwed.co.jp\z/
   validates :email, presence: true, uniqueness: true, format: { with: VALID_DOMAIN_REGEX }
+  has_many :events
 end
