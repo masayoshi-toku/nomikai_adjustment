2e62fe4eb22fc987934c4fa17a0365ecddf6ea42
masayoshi-toku <39050027+masayoshi-toku@users.noreply.github.com>
2018-06-13 15:53:45 +0900

    Add sesssions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,4 +2,29 @@ class SessionsController < ApplicationController
   def new
     @user = User.new
   end
+
+  def create
+    @user = User.find_or_create_by(user_params)
+
+    if @user.valid?
+      login_and_redirect @user, notice: '成功しました。'
+    else
+      redirect_back(fallback_location: root_path, notice: '無効なアカウントです。')
+    end
+  end
+
+  private
+
+    def user_params
+      { name: request.env['omniauth.auth']['info']['name'], email: request.env['omniauth.auth']['info']['email'] }
+    end
+
+    def request_from
+      request.env['omniauth.params']['request_from']
+    end
+
+    def login_and_redirect(user, notice)
+      session[:user_id] = user.id
+      redirect_to user, notice: notice
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   root to: 'sessions#new'
   resources :users, only: [:index, :show, :destroy]
   get "/auth/google_oauth2", as: "google_auth"
-  get '/auth/:provider/callback', to: 'google_omniauths#new'
+  get '/auth/:provider/callback', to: 'sessions#create'
   get '/login', to: 'sessions#new'
 end
