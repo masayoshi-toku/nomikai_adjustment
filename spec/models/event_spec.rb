830a78c901b8e5d7528a9e0a32fc1ea8b8603705
masayoshi-toku <39050027+masayoshi-toku@users.noreply.github.com>
2018-06-14 14:33:07 +0900

    Add test for url_path

--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -14,14 +14,26 @@ RSpec.describe Event, type: :model do
       end
 
       context "タイトルが100文字以上の時" do
-        let(:empty_name_event) { build(:event, title: 'これは１５文字のタイトルです。' * 7) }
-        it { expect(empty_name_event).to be_invalid }
+        let(:long_title_event) { build(:event, title: 'これは１５文字のタイトルです。' * 7) }
+        it { expect(long_title_event).to be_invalid }
       end
 
       context "同じユーザーが同じタイトルのイベントを作った時" do
         let(:event) { create(:event) }
         let(:dup_event) { event.dup }
         it { expect(dup_event).to be_invalid }
+      end
+
+      context "イベント用のURLが無い時" do
+        let(:empty_url_event) { build(:event, url_path: '') }
+        it { expect(empty_url_event).to be_invalid }
+      end
+
+      context "イベントURLが重複した時" do
+        before { create(:event) }
+        other_user = User.create(name: 'Mr.other_example', email: 'other_example@mwed.co.jp')
+        let(:same_url_event) { build(:event, user_id: other_user.id, title: '第1回飲み会（仮）') }
+        it { expect(same_url_event).to be_invalid }
       end
     end
   end
