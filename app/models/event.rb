424d1ade8b41dd931952b7a38230a6cf33a1b65d
masayoshi-toku <39050027+masayoshi-toku@users.noreply.github.com>
2018-06-14 14:32:18 +0900

    Add validation for url_path

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,5 @@
 class Event < ApplicationRecord
-  validates :title, presence: true, length: { maximum: 100 }, uniqueness: { scope: [:user_id, :title]  }
+  validates :title, presence: true, length: { maximum: 100 }, uniqueness: { scope: [:user_id, :title] }
+  validates :url_path, presence: true, uniqueness: true
   belongs_to :user
 end
