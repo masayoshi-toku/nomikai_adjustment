ee7443ad435edc90b0e261fa1a4434dfc12af31f
masayoshi-toku <39050027+masayoshi-toku@users.noreply.github.com>
2018-06-15 18:12:52 +0900

    Modify data type to datetime

--- db/migrate/20180615042525_create_event_dates.rb
+++ db/migrate/20180615042525_create_event_dates.rb
@@ -2,7 +2,11 @@ class CreateEventDates < ActiveRecord::Migration[5.2]
   def change
     create_table :event_dates do |t|
       t.references :event
+<<<<<<< HEAD
       t.string :event_date
+=======
+      t.datetime :event_date
+>>>>>>> Modify data type to datetime
 
       t.timestamps
     end
--- spec/factories/event_date_factory.rb
+++ spec/factories/event_date_factory.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :event_date do
-    event_date '2018-01-01'
+    event_date '2018-01-01'.to_time
     association :event, factory: :event
   end
 end
