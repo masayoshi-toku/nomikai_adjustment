f66c523cd081347fbda8a613e408c333b6d03aa7
masayoshi-toku <39050027+masayoshi-toku@users.noreply.github.com>
2018-06-19 16:07:29 +0900

    Modify controller's test by using url_path

--- spec/controllers/events_controller_spec.rb
+++ spec/controllers/events_controller_spec.rb
@@ -15,110 +15,161 @@ RSpec.describe EventsController, type: :controller do
   describe "GET #show" do
     subject { get :show, params: params }
 
-    context "イベントが存在する場合" do
-      let(:params) { { id: event.id } }
+    context "ログイン済みの場合" do
+      before do
+        event
+        log_in(event.user)
+      end
 
-      it { is_expected.to be_successful }
+      context "イベントが存在する場合" do
+        let(:params) { { url_path: event.url_path } }
+
+        it { is_expected.to be_successful }
+      end
+
+      context "イベントが存在しない場合" do
+        let(:params) { { url_path: event.url_path.split('').shuffle.join } }
+
+        it { is_expected.to redirect_to events_url }
+      end
     end
 
-    context "イベントが存在しない場合" do
-      let(:params) { { id: event.id + 1 } }
+    context "ログインしていない場合" do
+      let(:params) { { url_path: event.url_path } }
 
-      it { is_expected.to redirect_to events_url }
+      it { is_expected.to redirect_to root_url }
     end
   end
 
   describe "GET #new" do
     subject { get :new }
 
-    it { is_expected.to be_successful }
+    context "ログイン済みの場合" do
+      before do
+        event
+        log_in(event.user)
+      end
+
+      it { is_expected.to be_successful }
+    end
+
+    context "ログインしていない場合" do
+      it { is_expected.to redirect_to root_url }
+    end
   end
 
   describe "GET #edit" do
     subject { get :edit, params: params }
 
-    context "イベントが存在する場合" do
-      let(:params) { { id: event.id } }
+    context "ログイン済みの場合" do
+      before do
+        event
+        log_in(event.user)
+      end
 
-      it { is_expected.to be_successful }
+      context "イベントが存在する場合" do
+        let(:params) { { url_path: event.url_path } }
+
+        it { is_expected.to be_successful }
+      end
+
+      context "イベントが存在しない場合" do
+        let(:params) { { url_path: event.url_path.split('').shuffle.join } }
+
+        it { is_expected.to redirect_to events_url }
+      end
     end
 
-    context "イベントが存在しない場合" do
-      let(:params) { { id: event.id + 1 } }
+    context "ログインしていない場合" do
+      let(:params) { { url_path: event.url_path } }
 
-      it { is_expected.to redirect_to events_url }
+      it { is_expected.to redirect_to root_url }
     end
   end
 
   describe "POST #create" do
-    context "正しいパラメーターの場合" do
-      subject { proc { post :create, params: { event_form: valid_attributes } } }
-      before { log_in(user) }
+    context "ログイン済みの場合" do
+      before do
+        event
+        log_in(event.user)
+      end
 
-      it { is_expected.to change{ Event.count }.by(1) }
+      context "正しいパラメーターの場合" do
+        subject { proc { post :create, params: { event_form: valid_attributes } } }
 
-      it "イベントの詳細ページへリダイレクトする" do
-        subject.call
-        event = Event.find_by(title: valid_attributes[:title])
-        expect(response).to redirect_to event
+        it { is_expected.to change{ Event.count }.by(1) }
+
+        it "イベントの詳細ページへリダイレクトする" do
+          subject.call
+          event = Event.find_by(title: valid_attributes[:title])
+          expect(response).to redirect_to event.url_path
+        end
+      end
+
+      context "不正なパラメーターの場合" do
+        subject { post :create, params: { event_form: attributes } }
+
+        context "タイトルが空の場合" do
+          let(:attributes) { { title: '', event_dates_text: "2018/06/18\r\n2018/06/19\r\n2018/06/20" } }
+
+          it { is_expected.to render_template(:new) }
+        end
       end
     end
 
-    context "不正なパラメーターの場合" do
-      subject { post :create, params: { event_form: attributes } }
+    context "ログインしていない場合" do
+      subject { post :create, params: { event_form: valid_attributes } }
 
-      context "作成者が不明な場合" do
-        let(:attributes) { { title: '飲み会テストタイトル', event_dates_text: "2018/06/18\r\n2018/06/19\r\n2018/06/20" } }
-
-        it { is_expected.to render_template(:new) }
-      end
-
-      context "タイトルが空の場合" do
-        before { log_in(user) }
-        let(:attributes) { { title: '', event_dates_text: "2018/06/18\r\n2018/06/19\r\n2018/06/20" } }
-
-        it { is_expected.to render_template(:new) }
-      end
+      it { is_expected.to redirect_to root_path }
     end
   end
 
   describe "PUT #update" do
-    context "正しいパラメーターの場合" do
+    context "ログイン済みの場合" do
       before do
-        old_title
-        put :update, params: { id: event.id, event_form: attributes }
+        event
+        log_in(event.user)
       end
 
-      let(:old_title) { event.title }
-      let(:attributes) { { title: '新飲み会テストタイトル' } }
-      let(:updated_event) { Event.find_by(title: attributes[:title]) }
-
-      it { expect(updated_event.title).not_to eq old_title }
-
-      it { is_expected.to redirect_to updated_event }
-    end
-
-    context "不正なパラメーターの場合" do
-      subject { put :update, params: { id: event_id, event_form: attributes } }
-
-      context "イベントが存在しない場合" do
+      context "正しいパラメーターの場合" do
         before do
-          event
-          log_in(event.user)
+          old_title
+          put :update, params: { url_path: event.url_path, event_form: attributes }
         end
 
-        let(:attributes) { { title: '飲み会テストタイトル' } }
-        let(:event_id) { event.id + 1 }
+        let(:old_title) { event.title }
+        let(:attributes) { { title: '新飲み会テストタイトル' } }
+        let(:updated_event) { Event.find_by(url_path: event.url_path) }
 
-        it { is_expected.to render_template(:edit) }
+        it { expect(updated_event.title).not_to eq old_title }
+
+        it { is_expected.to redirect_to updated_event }
       end
 
-      context "タイトルが空の場合" do
-        let(:attributes) { { title: '' } }
-        let(:event_id) { event.id }
+      context "不正なパラメーターの場合" do
+        subject { put :update, params: { url_path: event_url_path, event_form: attributes } }
 
-        it { is_expected.to render_template(:edit) }
+        context "イベントが存在しない場合" do
+          let(:attributes) { { title: '飲み会テストタイトル' } }
+          let(:event_url_path) { event.url_path.split('').to_a.shuffle.join }
+
+          it { is_expected.to render_template(:edit) }
+        end
+
+        context "タイトルが空の場合" do
+          let(:attributes) { { title: '' } }
+          let(:event_url_path) { event.url_path }
+
+          it { is_expected.to render_template(:edit) }
+        end
       end
+    end
+
+    context "ログインしていない場合" do
+      subject { put :update, params: { url_path: event.url_path, event_form: attributes } }
+      let(:attributes) { { title: '飲み会テストタイトル' } }
+
+      it { is_expected.to redirect_to root_path }
     end
   end
 
@@ -126,18 +177,31 @@ RSpec.describe EventsController, type: :controller do
     subject { delete :destroy, params: params }
     before{ event }
 
-    context "イベントが存在する場合" do
-      let(:params) { { id: event.id } }
+    context "ログイン済みの場合" do
+      before do
+        event
+        log_in(event.user)
+      end
 
-      it { expect{ subject }.to change{ Event.count }.by(-1) }
+      context "イベントが存在する場合" do
+        let(:params) { { url_path: event.url_path } }
 
-      it { is_expected.to redirect_to events_url }
+        it { expect{ subject }.to change{ Event.count }.by(-1) }
+
+        it { is_expected.to redirect_to events_url }
+      end
+
+      context "イベントが存在しない場合" do
+        let(:params) { { url_path: event.url_path.split('').to_a.shuffle.join } }
+
+        it { is_expected.to render_template(:index) }
+      end
     end
 
-    context "イベントが存在しない場合" do
-      let(:params) { { id: event.id + 1 } }
+    context "ログインしていない場合" do
+      let(:params) { { url_path: event.url_path } }
 
-      it { is_expected.to render_template(:index) }
+      it { is_expected.to redirect_to root_url }
     end
   end
 end
