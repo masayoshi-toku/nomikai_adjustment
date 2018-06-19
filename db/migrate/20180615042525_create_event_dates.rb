4c1dd2168a1f2b6a25dcc8cd6a381b8f82d9e02c
masayoshi-toku <39050027+masayoshi-toku@users.noreply.github.com>
2018-06-19 13:29:32 +0900

    Revert "Modify data type to datetime"
    This reverts commit a18360a3120b254dd460a06bd6686e5787761339.

--- db/migrate/20180615042525_create_event_dates.rb
+++ db/migrate/20180615042525_create_event_dates.rb
@@ -2,11 +2,7 @@ class CreateEventDates < ActiveRecord::Migration[5.2]
   def change
     create_table :event_dates do |t|
       t.references :event
-<<<<<<< HEAD
       t.string :event_date
-=======
-      t.datetime :event_date
->>>>>>> Modify data type to datetime
 
       t.timestamps
     end
--- spec/factories/event_date_factory.rb
+++ spec/factories/event_date_factory.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :event_date do
-    event_date '2018-01-01'.to_time
+    event_date '2018-01-01'
     association :event, factory: :event
   end
 end
