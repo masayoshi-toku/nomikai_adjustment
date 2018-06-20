a6830ac3d98223c32e6b270ae6c205e19b301b36
masayoshi-toku <39050027+masayoshi-toku@users.noreply.github.com>
2018-06-20 13:39:25 +0900

    Modify to use various new line codes as the test varibale

--- spec/models/event_form_spec.rb
+++ spec/models/event_form_spec.rb
@@ -7,11 +7,14 @@ RSpec.describe EventForm, type: :model do
     let(:user) { create(:user) }
 
     context "正しい値の場合" do
-      let(:attributes) { { title: "第一回飲み会", event_dates_text: "2018/06/18\r\n2018/06/19\r\n2018/06/20" } }
+      let(:attributes) {
+          { title: "第一回飲み会",
+            event_dates_text: "6/18\r06/19\n06/20\r\n6/21\r\n\r\n6/22\n\n\n6/23\r\r\r\r6/24" }
+        }
 
       it { is_expected.to be_truthy }
       it { expect { subject }.to change { Event.count }.by(1) }
-      it { expect { subject }.to change { EventDate.count }.by(3) }
+      it { expect { subject }.to change { EventDate.count }.by(7) }
 
       context "タイトルの文字数が100文字の場合" do
         let(:attributes) { { title: "これは１０文字です。" * 10, event_dates_text: "2018/06/18\r\n2018/06/19\r\n2018/06/20" } }
