8f36852565ebc6f51cbef4d0bd9b728864e14f77
masayoshi-toku <39050027+masayoshi-toku@users.noreply.github.com>
2018-06-21 17:37:04 +0900

    Use delegate method

--- app/models/reaction.rb
+++ app/models/reaction.rb
@@ -1,5 +1,6 @@
 class Reaction < ApplicationRecord
   belongs_to :user
   belongs_to :event_date
+  delegate :event, to: :event_date
   validates :status, presence: true, inclusion: { in: 1..3 }
 end
