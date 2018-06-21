075430065272f92459210307cf684278b2c09c97
masayoshi-toku <39050027+masayoshi-toku@users.noreply.github.com>
2018-06-21 17:36:43 +0900

    Add reaction controller

---
+++ app/controllers/reactions_controller.rb
@@ -0,0 +1,52 @@
+class ReactionsController < ApplicationController
+  before_action :set_reaction, only: [:edit, :update, :destroy]
+
+  def new
+    @reaction = Reaction.new
+  end
+
+  def edit
+    exist_or_redirect(@reaction)
+  end
+
+  def create
+    @reaction = Reaction.new(reaction_params)
+
+    if @reaction.save
+      redirect_to event_url(@reaction.event), notice: '回答しました。'
+    else
+      render :new
+    end
+  end
+
+  def update
+    if @reaction&.update(reaction_params)
+      redirect_to event_url(@reaction.event), notice: 'Reaction was successfully updated.'
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    if @reaction&.destroy
+      redirect_to event_url(@reaction.event), notice: 'Reaction was successfully destroyed.'
+    else
+      redirect_to root_url
+    end
+  end
+
+  private
+    def set_reaction
+      @reaction = Reaction.find_by(id: params[:id])
+    end
+
+    def reaction_params
+      params.require(:reaction).permit(:user_id, :event_date_id, :status)
+    end
+
+    def exist_or_redirect(reaction)
+      unless reaction
+        redirect_back(fallback_location: root_path, notice: 'この回答は存在しません。')
+      end
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root to: 'sessions#new'
   resources :users, only: [:index, :show, :destroy]
   resources :events, param: 'url_path'
+  resources :reactions, except: [:index, :show]
   get "/auth/google_oauth2", as: "google_auth"
   get '/auth/:provider/callback', to: 'sessions#create'
   get '/login', to: 'sessions#new'
