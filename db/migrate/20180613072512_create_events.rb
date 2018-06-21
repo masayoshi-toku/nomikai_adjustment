e1bf0f22317b4fb69bde03797fcb742814235b21
masayoshi-toku <39050027+masayoshi-toku@users.noreply.github.com>
2018-06-21 11:58:55 +0900

    Modify to specify foreign_key

--- db/migrate/20180613072512_create_events.rb
+++ db/migrate/20180613072512_create_events.rb
@@ -1,12 +1,12 @@
 class CreateEvents < ActiveRecord::Migration[5.2]
   def change
     create_table :events do |t|
-      t.references :user
+      t.references :user, foreign_key: true
       t.string :title
 
       t.timestamps
     end
-    
+
     add_index :events, [:user_id, :title], unique: true
   end
 end
