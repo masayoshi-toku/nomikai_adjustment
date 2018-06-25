3874e31df20bd4a1cbebd9a77242bc9fde624122
masayoshi-toku <39050027+masayoshi-toku@users.noreply.github.com>
2018-06-25 18:25:31 +0900

    Refactor controller spec

--- spec/controllers/reactions_controller_spec.rb
+++ spec/controllers/reactions_controller_spec.rb
@@ -2,12 +2,12 @@ require 'rails_helper'
 
 RSpec.describe ReactionsController, type: :controller do
   let(:user) { create(:user) }
-  let(:event_date) { create(:event_date) }
-  let(:reaction) { create(:reaction) }
+  let(:event) { create(:event) }
+  let(:event_date) { create(:event_date, event: event) }
+  let(:reaction) { create(:reaction, user: user, event_date: event_date) }
 
   describe "GET #new" do
     subject { get :new, params: { url_path: url_path } }
-    let(:event) { create(:event) }
     let(:url_path) { event.url_path }
     it { is_expected.to be_successful }
   end
@@ -35,26 +35,25 @@ RSpec.describe ReactionsController, type: :controller do
     end
 
     context "正しい値の場合" do
-      let(:attributes) { { event_url_path: event_date.event.url_path, status: { "#{event_date.id}": '3' } } }
+      let(:attributes) { { event_url_path: event.url_path, answer: { "#{event_date.id}": '3' } } }
       it { expect{ subject }.to change{ Reaction.count }.by(1) }
 
       it "イベント詳細ページへリダイレクトする" do
-        event_url_path = event_date.event.url_path
+        event_url_path = event.url_path
         expect(subject).to redirect_to event_path(event_url_path)
       end
     end
 
     context "不正な値の場合" do
-      let(:attributes) { { event_url_path: event_date.event.url_path, status: { "#{event_date.id}": '10' } } }
+      let(:attributes) { { event_url_path: event.url_path, answer: { "#{event_date.id}": '10' } } }
 
-      it { is_expected.to redirect_to new_reaction_url(url_path: event_date.event.url_path) }
+      it { is_expected.to redirect_to new_reaction_url(url_path: event.url_path) }
     end
   end
 
   describe "DELETE #destroy" do
     subject { delete :destroy, params: { id: id } }
     before { reaction }
-    let(:event) { reaction.event }
 
     context "回答が存在する場合" do
       let(:id) { reaction.id }
