21fa1ac7df2ce229b19f3674a746c9e3bbace4ab
masayoshi-toku <39050027+masayoshi-toku@users.noreply.github.com>
2018-06-26 18:03:52 +0900

    Add helper for reaction index

---
+++ app/helpers/events_helper.rb
@@ -0,0 +1,48 @@
+module EventsHelper
+  def set_event
+    @event = Event.find_by(url_path: params[:url_path])
+  end
+
+  def answerers(event)
+    @answerers = []
+    first_event_date = event.event_dates.first
+    first_event_date_reactions = Reaction.where(event_date_id: event_date.id)
+
+    first_event_date_reactions.each do |first_event_date_reaction|
+      @answerers << first_event_date_reaction.user.name
+    end
+
+    @answerers
+  end
+
+  def reactions(event)
+    @reactions = []
+    event.event_dates.order('created_at ASC').each_with_index do |event_date, index|
+      @reactions[index] = {}
+      @reactions[index][:event_date] = event_date.event_date
+      @reactions[index][:marks] = []
+      @available = 0
+      @undecided = 0
+      @unavailable = 0
+
+      event_date.reactions.each do |reaction|
+        case reaction.status
+        when 1
+          mark = '◯'
+          @available += 1
+        when 2
+          mark = '△'
+          @undecided += 1
+        when 3
+          mark = '×'
+          @unavailable += 1
+        end
+
+        @reactions[index][:marks] << mark
+      end
+
+      @reactions[index][:counts] = [ @available, @undecided, @unavailable ]
+    end
+    @reactions
+  end
+end
