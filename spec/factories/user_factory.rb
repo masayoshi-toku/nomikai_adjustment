3b34a5fb5394f2fd0fc2280f6dcb6bbe3026cce1
masayoshi-toku <39050027+masayoshi-toku@users.noreply.github.com>
2018-06-26 13:27:20 +0900

    Refactor how to preapre multiple test data

--- spec/factories/user_factory.rb
+++ spec/factories/user_factory.rb
@@ -1,14 +1,6 @@
 FactoryBot.define do
-  sequence :email do |n|
-    "example#{n}@mwed.co.jp"
-  end
-
-  sequence :name do |n|
-    "Mr.example#{n}"
-  end
-
   factory :user do
-    name
-    email
+    sequence(:email) { |n| "example#{n}@mwed.co.jp" }
+    sequence(:name) { |n| "Mr.example#{n}" }
   end
 end
