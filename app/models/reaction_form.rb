bb2de8a499db5a4b7b787144377dca4f1a6083a3
masayoshi-toku <39050027+masayoshi-toku@users.noreply.github.com>
2018-06-28 15:26:47 +0900

    Add update method to reaction form

--- app/models/reaction_form.rb
+++ app/models/reaction_form.rb
@@ -11,11 +11,25 @@ class ReactionForm
     create_reaction
   end
 
+  def update
+    return false if invalid?
+    update_reaction
+  end
+
   private
     def create_reaction
       answer.each do |event_date_id, status|
         user.reactions.new(event_date_id: event_date_id, status: status)
       end
       user.save
+    end
+
+    def update_reaction
+      answer.each do |event_date_id, status|
+        reaction = user.reactions.find_by(event_date_id: event_date_id)
+        unless reaction.update(status: status)
+          return false
+        end
+      end
     end
 end
