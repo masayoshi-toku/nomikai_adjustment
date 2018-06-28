7ed8f2fcff4f314cbc6453bbcc7732a71b96b16f
masayoshi-toku <39050027+masayoshi-toku@users.noreply.github.com>
2018-06-28 13:08:09 +0900

    Remove destroy action and add initalize form object method

--- app/controllers/reactions_controller.rb
+++ app/controllers/reactions_controller.rb
@@ -1,17 +1,12 @@
 class ReactionsController < ApplicationController
-  before_action :set_reaction, only: [:edit, :update, :destroy]
-  before_action :set_event, only: [:new, :create]
+  before_action :set_event, only: [:new, :edit, :create]
 
   def new
-    if @event
-      @reaction_form = ReactionForm.new(reaction: Reaction.new)
-    else
-      redirect_to root_url
-    end
+    initialize_form_object
   end
 
   def edit
-    exist_or_redirect(@reaction)
+    initialize_form_object
   end
 
   def create
@@ -23,17 +18,13 @@ class ReactionsController < ApplicationController
     end
   end
 
-  def destroy
-    if @reaction&.destroy
-      redirect_to event_url(@reaction.event), notice: '回答を削除しました。'
-    else
-      redirect_to root_url, notice: '回答を削除できませんでした'
-    end
-  end
-
   private
-    def set_reaction
-      @reaction = Reaction.find_by(id: params[:id])
+    def initialize_form_object
+      if @event
+        @reaction_form = ReactionForm.new(reaction: Reaction.new)
+      else
+        redirect_to root_url, notice: 'イベントが存在しません。'
+      end
     end
 
     def set_event
@@ -42,11 +33,5 @@ class ReactionsController < ApplicationController
 
     def reaction_params
       params.require(:reaction_form).permit(answer: {})
-    end
-
-    def exist_or_redirect(reaction)
-      unless reaction
-        redirect_back(fallback_location: root_path, notice: 'この回答は存在しません。')
-      end
     end
 end
