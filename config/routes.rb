b60d233ecd5b6b9440e742bba83efafefcff6963
masayoshi-toku <39050027+masayoshi-toku@users.noreply.github.com>
2018-06-28 13:08:23 +0900

    Modify routing

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,9 @@ Rails.application.routes.draw do
   root to: 'sessions#new'
   resources :users, only: [:index, :show, :destroy]
   resources :events, param: 'url_path' do
-    resources :reactions, except: [:index, :show]
+    resource :reaction
   end
-  get "/auth/google_oauth2", as: "google_auth"
+  get '/auth/google_oauth2', as: 'google_auth'
   get '/auth/:provider/callback', to: 'sessions#create'
   get '/login', to: 'sessions#new'
   delete '/logout', to: 'sessions#destroy'
