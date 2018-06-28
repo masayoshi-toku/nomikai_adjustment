88a948e1cb8b93988fc7549ff0a3f653f10b51e1
masayoshi-toku <39050027+masayoshi-toku@users.noreply.github.com>
2018-06-28 20:13:49 +0900

    Add test pattern for count status

--- spec/models/event_date_spec.rb
+++ spec/models/event_date_spec.rb
@@ -36,14 +36,62 @@ RSpec.describe EventDate, type: :model do
 
   describe "#count_status" do
     subject { event_date.count_status }
-    before do
-      reaction
-      second_reaction
-    end
-    let(:event_date) { create(:event_date) }
-    let(:reaction) { create(:reaction, event_date: event_date, status: 1) }
-    let(:second_reaction) { create(:reaction, event_date: event_date, status: 2) }
 
-    it { is_expected.to eq [1, 1, 0]}
+    let(:event_date) { create(:event_date) }
+    let(:reaction) { create(:reaction, event_date: event_date, status: status) }
+    let(:second_reaction) { create(:reaction, event_date: event_date, status: second_status) }
+    let(:third_reaction) { create(:reaction, event_date: event_date, status: third_status) }
+
+    context "回答者がいる時" do
+      before do
+        reaction
+        second_reaction
+        third_reaction
+      end
+
+      context "全てが'◯'の時" do
+        let(:status) { 1 }
+        let(:second_status) { 1 }
+        let(:third_status) { 1 }
+
+        it { is_expected.to eq [3, 0, 0]}
+      end
+
+      context "全てが'△'の時" do
+        let(:status) { 2 }
+        let(:second_status) { 2 }
+        let(:third_status) { 2 }
+
+        it { is_expected.to eq [0, 3, 0]}
+      end
+
+      context "全てが'×'の時" do
+        let(:status) { 3 }
+        let(:second_status) { 3 }
+        let(:third_status) { 3 }
+
+        it { is_expected.to eq [0, 0, 3]}
+      end
+
+      context "全てのstatusが選択されている時" do
+        let(:status) { 1 }
+        let(:second_status) { 2 }
+        let(:third_status) { 3 }
+
+        it { is_expected.to eq [1, 1, 1]}
+      end
+
+      context "複数statusが重複する時" do
+        let(:status) { 1 }
+        let(:second_status) { 1 }
+        let(:third_status) { 3 }
+
+        it { is_expected.to eq [2, 0, 1]}
+      end
+    end
+
+    context "回答者がいない時" do
+      it { is_expected.to eq [0, 0, 0]}
+    end
   end
 end
