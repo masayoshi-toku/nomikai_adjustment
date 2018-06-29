066db8c2bb76e37d95ee0fc47bf540cd76fd26af
masayoshi-toku <39050027+masayoshi-toku@users.noreply.github.com>
2018-06-29 16:42:57 +0900

    Use transaction for update or create reaction method

--- app/models/reaction_form.rb
+++ app/models/reaction_form.rb
@@ -25,21 +25,20 @@ class ReactionForm
     end
 
     def update_or_create_reaction
-      @new_reaction_flag = 0
-      answer.each do |event_date_id, status|
-        reaction = user.reactions.find_by(event_date_id: event_date_id)
-        if reaction.present?
-          return false unless reaction.update(status: status)
-        else
-          @new_reaction_flag = 1
-          user.reactions.new(event_date_id: event_date_id, status: status)
+      ActiveRecord::Base.transaction do
+        answer.each do |event_date_id, status|
+          reaction = user.reactions.find_by(event_date_id: event_date_id)
+          if reaction.present?
+            reaction.status = status
+            reaction.save!
+          else
+            user.reactions.new(event_date_id: event_date_id, status: status)
+            user.save!
+          end
         end
       end
-
-      if @new_reaction_flag == 1
-        user.save
-      end
-
       return true
+    rescue
+      return false
     end
 end
