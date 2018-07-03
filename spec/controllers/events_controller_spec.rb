8ff0b97d18290744abac2a240a84de6372ce442f
masayoshi-toku <39050027+masayoshi-toku@users.noreply.github.com>
2018-07-03 17:18:48 +0900

    Modify the result of merge

--- spec/controllers/events_controller_spec.rb
+++ spec/controllers/events_controller_spec.rb
@@ -218,15 +218,13 @@ RSpec.describe EventsController, type: :controller do
               expect(updated_event.title).to eq attributes[:title]
             end
 
-            it { expect{ subject }.to change{ EventDate.count }.by(0) }
-          end
-
-          it :aggregate_failures do
-            expect{ subject }.to change{ EventDate.count }.by(0)
-            deleted_event_date = EventDate.find_by(id: event_date.id)
-            created_event_date = EventDate.find_by(event_date: other_event_date.event_date)
-            expect(deleted_event_date).to be nil
-            expect(created_event_date).not_to be nil
+            it :aggregate_failures do
+              expect{ subject }.to change{ EventDate.count }.by(0)
+              deleted_event_date = EventDate.find_by(id: event_date.id)
+              created_event_date = EventDate.find_by(event_date: other_event_date.event_date)
+              expect(deleted_event_date).to be nil
+              expect(created_event_date).not_to be nil
+            end
           end
         end
 
